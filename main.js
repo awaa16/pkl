544630a24d0a9973c9793186cbce85a30bf16ac6
awaa16 <syalwamadani16@gmail.com>
2024-09-04 08:30:21 +0700

    update

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('pesandisini')
+
+addEventListener('click', function() {
+    alert('Thank you for your interest! More information will be available soon.');
+    this.style.backgroundColor = '#27ae60';
+    this.innerText = 'Coming Soon!';
+});
+
